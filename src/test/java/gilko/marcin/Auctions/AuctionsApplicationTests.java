7a1a61b5313b68b23f89c5dddb39043e8cccd44a
gko <marcingilko@gmail.com>
2021-10-06 17:54:47 +0200

    add sping init files

---
+++ src/test/java/gilko/marcin/Auctions/AuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package gilko.marcin.Auctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
