1739297bc6bc415e1d3b36e36b27409ce6974c17
gko <marcingilko@gmail.com>
2021-10-24 17:32:42 +0200

    add save auction to controller

--- src/main/java/gilko/marcin/Auctions/controller/AuctionItemController.java
+++ src/main/java/gilko/marcin/Auctions/controller/AuctionItemController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import gilko.marcin.Auctions.auction.AuctionItem;
 import gilko.marcin.Auctions.service.AuctionItemService;
@@ -31,4 +33,12 @@ public class AuctionItemController {
 		
 		return "new_auction_item";
 	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String saveNewAuction(@ModelAttribute("auctionItem") AuctionItem auctionItem) {
+		service.save(auctionItem);
+		
+		return "redirect:/auctions";
+	}
+	
 }
