d351e59d7b1b6b24fcffdf7fb8ad04bea7a8eb4f
gko <marcingilko@gmail.com>
2021-10-24 19:06:02 +0200

    add new participant and save to its controller

--- src/main/java/gilko/marcin/Auctions/controller/ParticipantController.java
+++ src/main/java/gilko/marcin/Auctions/controller/ParticipantController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import gilko.marcin.Auctions.participant.Participant;
@@ -22,6 +23,25 @@ public class ParticipantController {
 		model.addAttribute("listParticipant", listParticipant);
 		
 		return "participants";
+	}
+	
+	@RequestMapping("/participants/new")
+	public String showNewParticipantPage(Model model) {
+		Participant participant = new Participant();
+		model.addAttribute("participant", participant);
+		
+		return "new_participant";
+	}
+	
+	@RequestMapping(value="participants/new/save", method = RequestMethod.POST)
+	public String saveNewParticipant(@Valid @ModelAttribute("participant") Participant participant,
+			BindingResult result) {
+		if(result.hasErrors()) {
+			return "new_participant";
+		}else {
+			service.save(participant);
+			return "redirect:/participants";
+		}
 	}
 	
 
