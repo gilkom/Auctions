1f6835c187883040574ea7e6b19cbc3626c3bff4
gko <marcingilko@gmail.com>
2021-10-30 11:03:21 +0200

    add all auctions controller

--- src/main/java/gilko/marcin/Auctions/controller/AuctionItemController.java
+++ src/main/java/gilko/marcin/Auctions/controller/AuctionItemController.java
@@ -50,4 +50,12 @@ public class AuctionItemController {
 
 	}
 	
+	@RequestMapping("/all_auctions")
+	public String viewAllAuctions(Model model) {
+		List<AuctionItem> listAuctionItems = service.listAll();
+		model.addAttribute("listAuctionItems", listAuctionItems);
+		
+		return "all_auctions";
+	}
+	
 }
